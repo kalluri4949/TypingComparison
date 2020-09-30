dac23f7893815ac6d3a41b612a513656eee45183
rakesh <kallurirakesh15@gmail.com>
2020-09-30 14:17:41 -0400

    Random sentence comparision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+  onUserTyped(char: string): void {
+    this.enteredText = char;
+  }
+  compare(singleChar: string, enterText: string): string {
+    if (!enterText) {
+      return 'pending';
+    }
+    return singleChar === enterText ? 'correct' : 'incorrect';
+  }
 }
